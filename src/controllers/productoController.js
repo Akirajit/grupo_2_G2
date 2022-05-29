f3b27b65f19aed71cead9691ebcbcfd2932d5ebc
AkiraJit <akirajit@gmail.com>
2022-05-29 17:53:40 -0300

    se cambio el json de productos a minusculas, se corrigió lo mismo en todos los llamados al json

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -23,15 +23,15 @@ const productoController = {
     },
     guardaProducto: function (req, res){
         let nuevaBirra = {
-            NOMBRE: req.body.nombre,
-            MARCA: req.body.marca,
-            DESCRIPCION: req.body.descripcion,
-            ABV: req.body.ABV,
-            IBU: req.body.IBU,
-            CONTENIDO: req.body.contenido,
-            PRECIO: req.body.precio,
-            STOCK: req.body.stock,
-            FOTO: '/imagenes/productos/'+req.file.filename,
+            nombre: req.body.nombre,
+            marca: req.body.marca,
+            descripcion: req.body.descripcion,
+            abv: req.body.abv,
+            ibu: req.body.ibu,
+            contenido: req.body.contenido,
+            precio: req.body.precio,
+            stock: req.body.stock,
+            foto: '/imagenes/productos/'+req.file.filename,
         }
         productos.push(nuevaBirra)
         const birraJSON = JSON.stringify(productos)
