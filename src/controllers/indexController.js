58c71b9cecdb50f349cd33c17910a938808be2f3
Martin Scalella <martinscalella@gmail.com>
2022-07-21 20:16:01 -0300

    crud en index

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,29 +1,46 @@
 //modulos
 const fs=require('fs');
 const path= require('path')
-
+const db= require("../database/models");
 //leyendo el json de productos
-const rutaProductos = path.join(__dirname,'../data/productos.json');
-let productos = JSON.parse(fs.readFileSync(rutaProductos,'utf-8'));
-const rutaUsuarios = path.join(__dirname, "../data/usuarios.json");
+// const rutaProductos = path.join(__dirname,'../data/productos.json');
+// let productos = JSON.parse(fs.readFileSync(rutaProductos,'utf-8'));
+// const rutaUsuarios = path.join(__dirname, "../data/usuarios.json");
 
-let usuarios = JSON.parse(fs.readFileSync(rutaUsuarios,'utf-8'));
+// let usuarios = JSON.parse(fs.readFileSync(rutaUsuarios,'utf-8'));
 
 const indexController = {
     home: function (req, res){
-        res.render('home' , {productos});
-    },
+        db.Producto.findAll({
+            include: ['marcas','tipos','contenido']
+        })
+        .then(productos => {
+            res.render('home',{productos})
+        })
+},
     quienes: function (req, res){
         res.render('quienes');
     },
-    home2: function (req, res){
-        res.render('home2');
-    },
+    // home2: function (req, res){
+    //     res.render('home2');
+    // },
     admin: function(req,res){
+        db.Producto.findAll({
+            include: ['marcas','tipos','contenido']
+        })
+        .then(productos => {
         res.render('admin' , {productos})
-    },
+    })},
+
     usuarios: function(req,res){
+        //hay que ver este controlador despues de hacer usuarios
+        db.Producto.findAll({
+            include: ['marcas','tipos','contenido']
+        })
+        .then(productos => {
         res.render('adminusr' , {usuarios})
+
+    })
     }
 }
 module.exports= indexController;
