7f4d392cbf2825212d67a740e47ad73d1162552c
AkiraJit <akirajit@gmail.com>
2022-08-23 00:37:07 -0300

    se arregló un middleware

--- src/middlewares/editarMiddleware.js
+++ src/middlewares/editarMiddleware.js
@@ -1,5 +1,11 @@
 function editarMidleware(req, res, next) {
-    if(res.locals.usuarioLogueado.isadmin!=1 && res.locals.usuarioLogueado.id_usuario != req.params.id) {
+
+    if(res.locals.usuarioLogueado==undefined) {
+        return res.redirect("http://localhost:4000/users/login")
+       
+    }
+
+    else if(res.locals.usuarioLogueado.isadmin!=1 && res.locals.usuarioLogueado.id_usuario != req.params.id) {
         return res.redirect("https://c.tenor.com/O5eQ19XUpSoAAAAC/psychedelic-trippy.gif")
        
     }
